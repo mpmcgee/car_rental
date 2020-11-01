53b2cb16c031d96d3d73a97ac2946e9218d1f393
mpmcgee <mpmcgee@iu.edu>
2020-11-01 10:08:45 -0500

    Reformatted for MVC

--- models/user_model.class.php
+++ models/user_model.class.php
@@ -1,12 +1,136 @@
 <?php
 /**
  * Author: Matthew McGee
- * Date: 10/31/2020
+ * Date: 10/30/2020
  * File: user_model.class.php
  *Description:
  */
 
+
 class UserModel
 {
+
+    private $db; //database object
+    private $dbConnection; // database connection object
+
+    public function __construct(){
+        $this->db = Database::getInstance();
+        $this->dbConnection = $this->db->getConnection();
+    }
+
+
+
+    public function add_user()
+    {
+        // REGISTER USER
+        if (isset($_POST['submit'])) {
+            // receive all input values from the form
+            $username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
+            $password = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
+            $password_hash = password_hash($password, PASSWORD_DEFAULT);
+            $email = filter_var($_POST['email'], FILTER_SANITIZE_STRING);
+            $first_name = filter_var(['first_name'], FILTER_SANITIZE_STRING);
+            $last_name = filter_var(['last_name'], FILTER_SANITIZE_STRING);
+        }
+        if (empty($username)) {
+
+            return false;
+
+        } else if (empty($password)) {
+
+            return false;
+
+        } else if (empty($email)) {
+
+            return false;
+
+        } else if (empty($first_name)) {
+
+            return false;
+
+        } else if (empty($last_name)) {
+
+            return false;
+
+        } else {
+            $sql = ("INSERT INTO" . $this->db->getUserTable() . "(username, password, email, first_name, last_name) 
+            VALUES ('$username', '$password_hash', '$email', '$first_name', '$last_name'");
+
+
+            if ($this->dbConnection->query($sql)) {
+
+                return true;
+            } else {
+
+                return false;
+            }
+        }
+
+    }
+    public function verify_user(){
+        if (isset($_POST['submit'])) {
+
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+
+            //sql select statement
+            $sql = "SELECT * FROM" . $this->db->getUserTable() . "WHERE username = '$username'";
+
+            //execute the query
+            $query = $this->dbConnection->query($sql);
+
+            if ($query && $query->num_rows > 0){
+                //array to store credentials from db
+                $credentials = array();
+
+                //loop through all rows
+                while ($query_row = $query->fetch_assoc()) {
+                    $credential = new User($query_row["username"],
+                        $query_row["password"]);
+
+                    //push credential into the array
+                    $credentials[] = $credential;
+                }
+            } if ($username == $credentials["username"] and password_verify($password, $credentials["password"])){
+                setcookie("login", $_REQUEST["username"]);
+
+            } else{
+                return false;
+            }
+        } else{
+
+            return false;
+        }
+    }
+
+    public function logout(){
+        setcookie('login', '', time()-70000000000, '/');
+    }
+
+    public function reset_password(){
+        if (isset($_COOKIE['login'])){
+            if (isset($_POST['submit'])) {
+                $username = $_POST['username'];
+                $password = $_POST['username'];
+                $password_hash = password_hash($password, PASSWORD_DEFAULT);
+
+                //sql select statement
+                $sql = "UPDATE" . $this->db->getUserTable() .
+                    "SET password = '$password_hash' where username = '$username'";
+
+                if ($this->dbConnection->query($sql) === TRUE){
+                    return true;
+                }
+
+                else{
+                    return false;
+                }
+
+
+            }
+        } else{
+            return false;
+        }
+    }
 
 }
--- models/vehicle_model.class.php
+++ models/vehicle_model.class.php
@@ -1,12 +1,57 @@
 <?php
 /**
  * Author: Matthew McGee
- * Date: 10/31/2020
- * File: vehicle_model.class.php
+ * Date: 10/30/2020
+ * File: toy_model.php
  *Description:
  */
 
-class VehicleModel
+class ToyModel
 {
+    private $db; //database object
+    private $dbConnection; // database connection object
 
+    public function __construct(){
+        $this->db = Database::getInstance();
+        $this->dbConnection = $this->db->getConnection();
+    }
+
+    /*
+ * this method retrieves all toys from the database and
+ * returns an array of Toy objects if successful or false if failed.
+ */
+    public function getToys() {
+        //SQL select statement
+        $sql = "SELECT * FROM " . $this->db->getVehiclesTable();
+
+        //execute the query
+        $query = $this->dbConnection->query($sql);
+
+        if ($query && $query->num_rows > 0) {
+            //array to store all toys
+            $vehicles = array();
+
+            //loop through all rows
+            while ($query_row = $query->fetch_assoc()) {
+                $vehicle = new Toy($query_row["vehicle_id"],
+                    $query_row["year"],
+                    $query_row["make"],
+                    $query_row["model"],
+                    $query_row["engine_type"],
+                    $query_row["transmission"],
+                    $query_row["class"],
+                    $query_row["doors"],
+                    $query_row["line"],
+                    $query_row["passengers"],
+                    $query_row["suitcases"],
+                    $query_row["sirius"],
+                    $query_row["price_per_day"]);
+
+                //push the toy into the array
+                $vehicles[] = $vehicle;
+            }
+            return $vehicles;
+        }
+        return false;
+    }
 }
