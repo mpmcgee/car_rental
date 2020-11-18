8a3f3b7311daa590b771fd91bc966f86b7ebbe3a
Boredanny <61812503+Boredanny@users.noreply.github.com>
2020-11-17 19:33:57 -0500

    user issues

--- views/user/user_index_view.class.php
+++ views/user/user_index_view.class.php
@@ -15,45 +15,28 @@ class UserIndexView extends IndexView
             //the media type
             var media = "user";
         </script>
-        <!-- page specific content starts -->
-        <!-- top row for the page header  -->
-        <div class="top-row">CREATE AN ACCOUNT</div>
-        <div class="header">
-            <h3>Please complete the entire form. All fields required.</h3>
-        </div>
-        <!-- middle row -->
-        <div class="middle-row">
-            <form method="POST" action="index.php?action=register">
-                <p>
-                    <input id="username" value="" name="username" type="text" required="required" placeholder="Username"/>
-                    <br>
-                </p>
-                <p>
-                    <input id="password" value="" name="password" type="text" required="required" placeholder="Password, 5 characters minimum"/>
-                    <br>
-                </p>
-                <p>
-                    <input id="email" value="" name="email" type="email" required="required" placeholder="Email"/>
-                    <br>
-                </p>
-                <p>
-                    <input id="firstname" value="" name="firstname" type="text" required="required" placeholder="First name"/>
-                    <br>
-                </p>
-                <p>
-                    <input id="lastname" value="" name="lastname" type="text" required="required" placeholder="Last name"/>
-                    <br>
-                </p>
-                <button type="submit" style="width: 560px; background-color: #333333; height: 50px; color: white"><span>Register</span></button>
-            </form>
-        </div>
-        <hr>
-        <!-- bottom row for links  -->
-        <div class="bottom-row">
-            <span style="float: left">Already have an account? <a href="<?= BASE_URL ?>/user/login">Login</a></span>
-        </div>
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Rental User Management System</title>
+        <link href="www/css/styles.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+    <h1><span style="color: forestgreen; font-family: serif; font-size: 36pt">Rental</span> User Management System</h1>
+    <div id="wrapper">
+        <img src="www/img/gfif.jpg" style="float: right; width: 130px">
         <?php
-    }
+        }
+
+        //create the page footer
+        public static function footer() {
+        ?>
+    </div>
+    </body>
+    </html>
+    <?php
+}
 
     public static function displayFooter() {
         parent::displayFooter();
