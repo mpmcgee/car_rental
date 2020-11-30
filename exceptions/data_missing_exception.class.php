e844774205a2062d10cf3447cd4f7731930ca12a
coltespich <73557995+coltespich@users.noreply.github.com>
2020-11-29 20:00:27 -0500

    Created exceptions classes

---
+++ exceptions/data_missing_exception.class.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Author: Coltin Espich
+Date: 11-29-2020
+Name: data_missing_exception.class.php
+Description: Exception for missing data.
+*/
+
+class DataMissingException extends Exception {
+    //Use default exception message.
+}
