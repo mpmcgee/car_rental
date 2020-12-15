f7449c6106ec35af14e9fc7e2c781d3fe990ddff
coltespich <73557995+coltespich@users.noreply.github.com>
2020-12-15 18:25:06 -0500

    Made the ajax results more specific

--- www/js/ajax_vehicleclass.js
+++ www/js/ajax_vehicleclass.js
@@ -1,11 +1,19 @@
 var numResults = 0;
 var suggestionBoxObj;
 var selectBoxObj;
+var startDate;
+var endDate;
+var carClass;
 var vLine;
+var splitString;
+var query;
+var results;
 
 //initial actions to take when the page load
 window.onload = function () {
     //DOM objects
+    startDate = document.getElementById('start-date');
+    endDate = document.getElementById('end-date');
     selectBoxObj = document.getElementById('class');
     suggestionBoxObj = document.getElementById('suggestionDiv');
     selectBoxObj.addEventListener('change', handleOnChange);
@@ -13,44 +21,62 @@ window.onload = function () {
 
 function handleOnChange() {
     //retrieve input from the select box
-    vLine = selectBoxObj.value;
+    splitString = (selectBoxObj.value).split(" ");
+    vLine = splitString[1];
+    carClass= splitString[0];
     console.log(vLine);
+    console.log(carClass)
+
+    query = carClass + "-" + vLine;
+    console.log(query);
 
     // create an XHR object
     var xhr = new XMLHttpRequest();
 
     //open an asynchronous AJAX request
-    xhr.open("GET", base_url + "/booking/getVline/" + vLine, true);
+    xhr.open("GET", base_url + "/booking/getVline/" + query, true);
 
     xhr.onload = function () {
         //handle server's responses
-        var results = JSON.parse(xhr.responseText);
-        console.log(results);
+        if (xhr.responseText == false) {
+            results = "No available vehicles";
+            displayVehicles(results);
+        } else {
+            results = JSON.parse(xhr.responseText);
+            console.log(results);
 
-        displayVehicles(results);
-    };
+            displayVehicles(results);
+        }
+
+    }
     xhr.send(null);
 }
 
 function displayVehicles(results) {
     numResults = results.length;
-
+    suggestionBoxObj.innerHTML = "";
+    
     if (numResults === 0) {
         //hide all suggestions
         suggestionBoxObj.style.display = 'none';
         return false;
     }
 
-    suggestionBoxObj.innerHTML = "";
-
-    for (let i = 0; i < numResults; i++) {
-        // console.log(results[i]);
-        suggestionBoxObj.innerHTML += "<input type='radio' id=" + results[i].vehicle_id + " value= " + results[i].vehicle_id + " name = pick_car>";
-        suggestionBoxObj.innerHTML += "<label for=" + results[i].vehicle_id + ">" + results[i].make + " " + results[i].model +
-            " (" + results[i].year +  ") - $" + results[i].price_per_day + "</label>";
-        suggestionBoxObj.innerHTML += "<br>";
+if (typeof (results) == "string") {
+        suggestionBoxObj.innerHTML = "<h3>Available \""+vLine+" "+ carClass+"s\"<h3>";
+        suggestionBoxObj.innerHTML += "<p>"+ results +"</p>";
+    } else {
+        suggestionBoxObj.innerHTML = "<h3>Available \"" + vLine + " " + carClass + "s\"<h3>";
+        for (let i = 0; i < numResults; i++) {
+            // console.log(results[i]);
+            suggestionBoxObj.innerHTML += "<input type='radio' id=" + results[i].vehicle_id + " value= " + results[i].vehicle_id + " name = pick_car>";
+            suggestionBoxObj.innerHTML += "<label for=" + results[i].vehicle_id + ">" + results[i].make + " " + results[i].model +
+                " (" + results[i].year + ") - $" + results[i].price_per_day + "</label>";
+            suggestionBoxObj.innerHTML += "<br>";
+        }
     }
     //display the spans in the div block
     suggestionBoxObj.style.display = 'block';
 
 }
+   
