d635b91d405d4bd9528c6a341be741c525a28975
Boredanny <61812503+Boredanny@users.noreply.github.com>
2020-12-15 13:50:54 -0500

    Finished Exceptions (please look over them if you get a chance)

--- models/booking_model.class.php
+++ models/booking_model.class.php
@@ -18,7 +18,8 @@ class BookingModel
 
     //To use singleton pattern, this constructor is made private.
     // To get an instance of the class, the getBookModel method must be called.
-    private function __construct(){
+    private function __construct()
+    {
         $this->db = Database::getDatabase();
         $this->dbConnection = $this->db->getConnection();
         $this->tblBookings = $this->db->getBookingsTable();
@@ -36,25 +37,25 @@ class BookingModel
         }
 
         //initializes the user
-        if(!isset($_SESSION['users'])){
+        if (!isset($_SESSION['users'])) {
             $users = $this->getBookingCustomers();
             $_SESSION['users'] = $users;
         }
 
         //initializes the vehicle
-        if(!isset($_SESSION['vehicles'])){
+        if (!isset($_SESSION['vehicles'])) {
             $vehicles = $this->getBookingVehicles();
             $_SESSION['vehicles'] = $vehicles;
         }
 
         //initializes the vehicle
-        if(!isset($_SESSION['makes'])){
+        if (!isset($_SESSION['makes'])) {
             $makes = $this->getBookingMakes();
             $_SESSION['makes'] = $makes;
         }
 
         //initializes the vehicle
-        if(!isset($_SESSION['models'])){
+        if (!isset($_SESSION['models'])) {
             $models = $this->getBookingModels();
             $_SESSION['models'] = $models;
         }
@@ -62,8 +63,9 @@ class BookingModel
     }
 
     //static method to ensure there is just one BookingModel instance
-    public static function getBookingModel(){
-        if (self::$_instance == NULL){
+    public static function getBookingModel()
+    {
+        if (self::$_instance == NULL) {
             self::$_instance = new BookingModel();
         }
         return self::$_instance;
@@ -75,12 +77,13 @@ class BookingModel
      * Bookings should also be filtered by user and vehicle and/or sorted by user or vehicle.
      */
 
-    public function list_booking(){
-            /* construct the sql SELECT statement in this format
-             * SELECT ...
-             * FROM ...
-             * WHERE ...
-             */
+    public function list_booking()
+    {
+        /* construct the sql SELECT statement in this format
+         * SELECT ...
+         * FROM ...
+         * WHERE ...
+         */
         try {
             $sql = "SELECT * FROM " . $this->tblBookings . "," . $this->tblUsers . "," .
                 $this->tblVehicles . " WHERE " . $this->tblBookings . ".user_id=" . $this->tblUsers .
@@ -169,7 +172,7 @@ class BookingModel
         } catch (Exception $e) {
             return $e->getMessage();
         }
-        }
+    }
 
     public function search_bookings($terms)
     {
@@ -252,43 +255,42 @@ class BookingModel
 
         try {
 
-                if (session_status() == PHP_SESSION_NONE) {
-                    session_start();
+            if (session_status() == PHP_SESSION_NONE) {
+                session_start();
 
+            }
+            if (isset($_SESSION['user_id'])) {
+                $user_id = $_SESSION['user_id'];
+
+
+                //query string for update
+                $sql = "INSERT INTO " . $this->tblBookings .
+                    " VALUES (NULL, '$vehicle_id', '$user_id', '$start_date', '$end_date')";
+
+                //execute the query and return true if successful or false if failed
+                $query = $this->dbConnection->query($sql);
+
+
+                if (!$query) {
+                    throw new DatabaseException("There was an error adding your booking.");
+                } else {
+                    return "Your booking was successfully created.";
                 }
-                if (isset($_SESSION['user_id'])) {
-                    $user_id = $_SESSION['user_id'];
-
-
-
-                    //query string for update
-                    $sql = "INSERT INTO " . $this->tblBookings .
-                        " VALUES (NULL, '$vehicle_id', '$user_id', '$start_date', '$end_date')";
-
-                    //execute the query and return true if successful or false if failed
-                    $query = $this->dbConnection->query($sql);
-
-
-                    if (!$query) {
-                        throw new DatabaseException("There was an error adding your booking.");
-                    } else{
-                        return "Your booking was successfully created.";
-                    }
-
-
-                    }
-                    }catch (DatabaseException $e) {
-                        return $e->getMessage();
-                    }
-
 
 
             }
+        } catch (DatabaseException $e) {
+            return $e->getMessage();
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
 
 
-                //the add booking method a new booking to the database. Details of the booking are posted in a form. Return true if succeed; false otherwise.
-                public function update_booking($id)
-        {
+    //the add booking method a new booking to the database. Details of the booking are posted in a form. Return true if succeed; false otherwise.
+    public function update_booking($id)
+    {
+        try {
             //if the script did not received post data, display an error message and then terminate the script immediately
             if (!filter_has_var(INPUT_POST, 'first-name') ||
                 !filter_has_var(INPUT_POST, 'last-name') ||
@@ -299,21 +301,28 @@ class BookingModel
                 return false;
             }
 
-            //retrieve data for the new movie; data are sanitized and escaped for security.
+            //retrieve data for the new booking; data are sanitized and escaped for security.
             $first_name = $this->dbConnection->real_escape_string(trim(filter_input(INPUT_POST, 'first-name', FILTER_SANITIZE_STRING)));
             $last_name = $this->dbConnection->real_escape_string(trim(filter_input(INPUT_POST, 'last-name', FILTER_SANITIZE_STRING)));
             $vehicle_id = $this->dbConnection->real_escape_string(filter_input(INPUT_POST, 'vehicle-id', FILTER_DEFAULT));
             $start_date = $this->dbConnection->real_escape_string(trim(filter_input(INPUT_POST, 'start-date', FILTER_SANITIZE_STRING)));
             $end_date = $this->dbConnection->real_escape_string(trim(filter_input(INPUT_POST, 'end-date', FILTER_SANITIZE_STRING)));
 
+            //Check for missing data.
+            if ($first_name == "" || $last_name == "" || $vehicle_id == "" || $start_date == "" || $end_date == "") {
+                throw new DataMissingException("Please complete all fields.");
+            }
 
             //query string for update
             $sql = "UPDATE " . $this->tblBookings .
                 " SET first_name='$first_name', last_name='$last_name', vehicle_id='$vehicle_id', start_date='$start_date', "
                 . "end_date='$end_date' WHERE id='$id'";
 
-            //execute the query
+            //execute the query and return true if successful or false if failed
             $query = $this->dbConnection->query($sql);
+            if (!$query) {
+                throw new DatabaseException("There was an error updating user to the database.");
+            }
 
             if (is_null($query)) {
                 return false;
@@ -323,23 +332,28 @@ class BookingModel
 
                 return true;
             }
+        } catch (DataMissingException $e) {
+            return $e->getMessage();
+        } catch (DatabaseException $e) {
+            return $e->getMessage();
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
     }
 
 
+    //get all users
 
-
-        //get all users
-
-        private function getBookingCustomers(){
-
+    private function getBookingCustomers()
+    {
+        try {
 
             $sql = "SELECT * FROM " . $this->tblUsers;
 
-            //execute the query
+            //execute the query and return true if successful or false if failed
             $query = $this->dbConnection->query($sql);
-
             if (!$query) {
-                return false;
+                throw new DatabaseException("There was an error getting customer from database.");
             }
 
             //loop through all rows
@@ -348,69 +362,92 @@ class BookingModel
                 $users[$obj->user_id] = $obj->user_id;
             }
             return $users;
+        } catch (DatabaseException $e) {
+            return $e->getMessage();
+        } catch (Exception $e) {
+            return $e->getMessage();
         }
+    }
 
 
     //get all vehicle
-    private function getBookingVehicles(){
-        $sql = "SELECT * FROM " . $this->tblVehicles;
+    private function getBookingVehicles()
+    {
+        try {
+            $sql = "SELECT * FROM " . $this->tblVehicles;
 
-        //execute the query
-        $query = $this->dbConnection->query($sql);
+            //execute the query and return true if successful or false if failed
+            $query = $this->dbConnection->query($sql);
+            if (!$query) {
+                throw new DatabaseException("There was an error getting the vehicle from the database.");
+            }
 
-        if (!$query){
-            return false;
+            //loop through all rows
+            $vehicles = array();
+            while ($obj = $query->fetch_object()) {
+                $vehicles[$obj->vehicle_id] = $obj->vehicle_id;
+
+
+            }
+            return $vehicles;
+        } catch (DatabaseException $e) {
+            return $e->getMessage();
+        } catch (Exception $e) {
+            return $e->getMessage();
         }
-
-        //loop through all rows
-        $vehicles = array();
-        while ($obj = $query->fetch_object()){
-            $vehicles[$obj->vehicle_id] = $obj->vehicle_id;
-
-
-        }
-        return $vehicles;
     }
 
     //get all makes
-    private function getBookingMakes(){
-        $sql = "SELECT * FROM " . $this->tblVehicles;
+    private function getBookingMakes()
+    {
+        try {
+            $sql = "SELECT * FROM " . $this->tblVehicles;
 
-        //execute the query
-        $query = $this->dbConnection->query($sql);
+            //execute the query and return true if successful or false if failed
+            $query = $this->dbConnection->query($sql);
+            if (!$query) {
+                throw new DatabaseException("There was an error getting the vehicle make from the database.");
+            }
 
-        if (!$query){
-            return false;
+            //loop through all rows
+            $makes = array();
+            while ($obj = $query->fetch_object()) {
+                $makes[$obj->make] = $obj->make;
+
+
+            }
+            return $makes;
+        } catch (DatabaseException $e) {
+            return $e->getMessage();
+        } catch (Exception $e) {
+            return $e->getMessage();
         }
-
-        //loop through all rows
-        $makes = array();
-        while ($obj = $query->fetch_object()){
-            $makes[$obj->make] = $obj->make;
-
-
-        }
-        return $makes;
     }
 
     //get all makes
-    private function getBookingModels(){
-        $sql = "SELECT * FROM " . $this->tblVehicles;
+    private function getBookingModels()
+    {
+        try {
+            $sql = "SELECT * FROM " . $this->tblVehicles;
 
-        //execute the query
-        $query = $this->dbConnection->query($sql);
+            //execute the query and return true if successful or false if failed
+            $query = $this->dbConnection->query($sql);
+            if (!$query) {
+                throw new DatabaseException("There was an error getting the vehicle model from the database.");
+            }
 
-        if (!$query){
-            return false;
+            //loop through all rows
+            $models = array();
+            while ($obj = $query->fetch_object()) {
+                $models[$obj->model] = $obj->model;
+
+
+            }
+            return $models;
+        } catch (DatabaseException $e) {
+            return $e->getMessage();
+        } catch (Exception $e) {
+            return $e->getMessage();
         }
-
-        //loop through all rows
-        $models = array();
-        while ($obj = $query->fetch_object()){
-            $models[$obj->model] = $obj->model;
-
-
-        }
-        return $models;
     }
 }
